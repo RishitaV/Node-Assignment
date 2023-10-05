ebd0c5b2c5e3bffe22995a657b136a65e8c7fb9c
RishitaV <rishitavalechha@gmail.com>
2023-10-05 09:24:48 +0530

    Assignment Day 1, deleted all files

--- node-training-assignment/src/user/userModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    userName: String,
-    userEmail: String,
-});
-
-module.exports = mongoose.model('User', userSchema);
